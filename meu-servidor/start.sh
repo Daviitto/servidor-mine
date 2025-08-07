958ddc9ca9727d4a201c04599743a8db91105b63
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 18:41:23 -0300

    Update start.sh

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -2,15 +2,13 @@
 
 echo "Baixando servidor Minecraft..."
 
-# Substitua com o ID do seu arquivo no Drive
+# ID do seu arquivo no Google Drive (já preenchido)
 FILE_ID="1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn"
 FILE_NAME="servidor.zip"
 
-# Baixa o arquivo lidando com a proteção do Google Drive
+# Baixa o ZIP do Google Drive, contornando confirmação
 curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=${FILE_ID}" -s > /tmp/intermezzo.html
-
 CODE="$(awk '/confirm=/{print $NF}' /tmp/intermezzo.html)"
-
 curl -Lb /tmp/cookies "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o "${FILE_NAME}"
 
 echo "Descompactando servidor..."
@@ -20,4 +18,5 @@ echo "Aceitando EULA..."
 echo "eula=true" > eula.txt
 
 echo "Iniciando servidor Minecraft..."
+# Ajuste abaixo se o seu .jar tiver um nome diferente de fabric-server-launch.jar
 java -Xmx4G -jar fabric-server-launch.jar nogui
