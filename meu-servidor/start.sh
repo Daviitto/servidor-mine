9764e2e0d2fc3a0fff7a6e7725937cc695b8dfbb
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 19:13:23 -0300

    Update start.sh

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -e
 
-echo "Baixando servidor Minecraft com gdown..."
 FILE_ID="1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn"
 FILE_NAME="servidor.zip"
-python3 -m gdown --id $FILE_ID -O $FILE_NAME
+
+cd /app
+
+# só baixa novamente se não existir
+if [ ! -f "$FILE_NAME" ]; then
+  echo "Baixando servidor Minecraft..."
+  curl -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" \
+       -o "${FILE_NAME}"
+fi
 
 echo "Descompactando servidor..."
 unzip -o "$FILE_NAME" -d .
 
-# Se o seu ZIP extrai para "./Server", navegue até lá:
+# entra na pasta onde foi extraído
 cd Server
 
 echo "Aceitando EULA..."
 echo "eula=true" > eula.txt
 
 echo "Iniciando servidor Minecraft..."
-# Usa exec para substituir o shell e manter o processo do Java em primeiro plano
 exec java -Xmx4G -jar fabric-server-launch.jar nogui
