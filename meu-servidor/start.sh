db81c02cb227b317e35c265b7dd6c484e3e87257
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 19:03:56 -0300

    Update start.sh

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -2,19 +2,19 @@
 set -e
 
 echo "Baixando servidor Minecraft com gdown..."
-
-# ID do seu arquivo no Drive
 FILE_ID="1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn"
 FILE_NAME="servidor.zip"
-
-# Baixa direto com gdown (ele lida com tokens de confirmação)
 python3 -m gdown --id $FILE_ID -O $FILE_NAME
 
 echo "Descompactando servidor..."
 unzip -o "$FILE_NAME" -d .
 
+# Se o seu ZIP extrai para "./Server", navegue até lá:
+cd Server
+
 echo "Aceitando EULA..."
 echo "eula=true" > eula.txt
 
 echo "Iniciando servidor Minecraft..."
-java -Xmx4G -jar fabric-server-launch.jar nogui
+# Usa exec para substituir o shell e manter o processo do Java em primeiro plano
+exec java -Xmx4G -jar fabric-server-launch.jar nogui
