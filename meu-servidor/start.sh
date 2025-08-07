6c8151dc2974ab9d84ea4ec3fb4be520e84d6279
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 19:49:33 -0300

    Update start.sh

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -1,12 +1,10 @@
-
 #!/bin/bash
 
 echo "Baixando servidor.zip..."
-gdown https://drive.google.com/uc?id=1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn -O servidor.zip
+wget -O servidor.zip "https://drive.usercontent.google.com/download?id=1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn&export=download"
 
-echo "Descompactando servidor.zip..."
-unzip servidor.zip
+echo "Descompactando servidor..."
+unzip servidor.zip || { echo "Erro ao descompactar o servidor."; exit 1; }
 
-echo "Iniciando servidor Minecraft..."
-cd servidor
+echo "Iniciando servidor..."
 java -Xmx4G -jar server.jar nogui
