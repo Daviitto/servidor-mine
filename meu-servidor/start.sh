c4e1d55ec3c3961b5dc1ea00b54b677018e5835c
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 19:41:22 -0300

    Update start.sh

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -1,24 +1,12 @@
+
 #!/bin/bash
 
-# Aceita o EULA automaticamente
-echo "eula=true" > eula.txt
-
-# Baixar servidor.zip do Google Drive
 echo "Baixando servidor.zip..."
-wget -O servidor.zip "https://drive.google.com/uc?export=download&id=1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn"
+gdown https://drive.google.com/uc?id=1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn -O servidor.zip
 
-# Verificar se o zip foi baixado corretamente
-if unzip -t servidor.zip > /dev/null 2>&1; then
-    echo "Arquivo ZIP válido. Descompactando..."
-    unzip -o servidor.zip -d Server
-else
-    echo "Erro: servidor.zip inválido ou corrompido."
-    exit 1
-fi
+echo "Descompactando servidor.zip..."
+unzip servidor.zip
 
-# Ir para a pasta do servidor
-cd Server || { echo "Pasta Server não encontrada."; exit 1; }
-
-# Iniciar servidor
 echo "Iniciando servidor Minecraft..."
-java -Xmx4G -jar fabric-server-launch.jar nogui
+cd servidor
+java -Xmx4G -jar server.jar nogui
