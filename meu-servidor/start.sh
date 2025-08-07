f5b7bb1d17a218900b64e2c8941c2035e2e28853
Davi Leal dos Santos Bastos <davibastos0309@gmail.com>
2025-08-07 18:19:33 -0300

    Add files via upload

--- meu-servidor/start.sh
+++ meu-servidor/start.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-FILE="server.zip"
+echo "Baixando servidor Minecraft..."
 
-# Se ainda não foi baixado, baixa o arquivo do Google Drive
-if [ ! -f "$FILE" ]; then
-  echo "Baixando servidor..."
-  curl -L -o "$FILE" "https://drive.google.com/uc?export=download&id=1ssIVv0bcUXVMUckjxUQNOMglysk38Yy2"
-fi
+# Substitua com o ID do seu arquivo no Drive
+FILE_ID="1UWps-gQutx2s3WqZY9uU7CAzjxSF0qrn"
+FILE_NAME="servidor.zip"
 
-# Descompacta os arquivos na pasta 'server'
-if [ ! -d "server" ]; then
-  unzip "$FILE" -d server
-fi
+# Baixa o arquivo lidando com a proteção do Google Drive
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=${FILE_ID}" -s > /tmp/intermezzo.html
 
-# Inicia o servidor
-cd server
+CODE="$(awk '/confirm=/{print $NF}' /tmp/intermezzo.html)"
+
+curl -Lb /tmp/cookies "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o "${FILE_NAME}"
+
+echo "Descompactando servidor..."
+unzip -o "${FILE_NAME}" -d .
+
+echo "Aceitando EULA..."
+echo "eula=true" > eula.txt
+
+echo "Iniciando servidor Minecraft..."
 java -Xmx4G -jar fabric-server-launch.jar nogui
